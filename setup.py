e71090e495db8dede3dbaa46b47ba874b70c06e1
Robert James <robert.james.19@ucl.ac.uk>
2026-05-14 16:16:52 +1000

    Make this installable as a submodule; update .gitignore.

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup, find_packages
 setup(
         name="fogtask",
         version = "0.3",
-        package_dir = {"fogtask":"fogtask"},
-        packages=find_packages(),
+        packages=["fogtask", "detector_parameters"],
+        package_dir = {"fogtask":"fogtask", "detector_parameters": "detector_parameters"},
+        include_package_data=True,
+        package_data={'detector_parameters': ['*.yaml']},
         author="Maike Doerenkamp,Robert James,Knut Morå",
         author_email="fysikk@dundasmora.no",
         description="Code for XLZD requirements task force",
